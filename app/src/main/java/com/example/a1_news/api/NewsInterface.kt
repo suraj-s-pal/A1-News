500dea123cdb608177aa8dceac4e1ab72e5574eb
suraj-s-pal <snarayanrpal@gmail.com>
2024-01-23 23:30:46 +0530

    all intended functions implemented

--- app/src/main/java/com/example/a1_news/api/NewsInterface.kt
+++ app/src/main/java/com/example/a1_news/api/NewsInterface.kt
@@ -11,8 +11,8 @@ const val BASE_URL = "https://newsapi.org"
 //const val API_KEY = "642867dd40174823808e138684afd0ff"
 //const val API_KEY = "6400ede5a803452888f745e0bea10eef"
 //const val API_KEY = "5ce582463e364b23958a220375d49296"
-//const val API_KEY = "91ec37dff4ab40319b9033cc3b1cb7d1"
-const val API_KEY = "a7d06964bf93483e9d0499e79e20c4cd"
+const val API_KEY = "87aaa9dfa8f249d4bea20460bb802357"
+//const val API_KEY = "a7d06964bf93483e9d0499e79e20c4cd"
 
 interface NewsInterface {
     @GET("v2/top-headlines?apiKey=$API_KEY")
@@ -24,6 +24,15 @@ interface NewsInterface {
         @Query("page")
         page: Int,
     ): Call<News>
+
+
+      /*  @GET("v2/everything?&from=2024-01&sortBy=relevancy&apiKey=$API_KEY")
+        fun searchNews(
+            @Query("q") query: String,
+        ): Call<News>*/
+
+    @GET("v2/everything?sortBy=relevancy&apiKey=$API_KEY")
+    fun searchNews(@Query("q") query: String): Call<News>
 }
 
 
--- app/src/main/java/com/example/a1_news/ui/MainActivity.kt
+++ app/src/main/java/com/example/a1_news/ui/MainActivity.kt
@@ -1,22 +1,24 @@
 package com.example.a1_news.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.a1_news.R
 import com.example.a1_news.adapter.NewsAdapter
 import com.example.a1_news.api.NewsService
-import com.example.a1_news.R
 import com.example.a1_news.model.Article
 import com.example.a1_news.model.News
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import androidx.appcompat.widget.SearchView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,17 +28,42 @@ class MainActivity : AppCompatActivity() {
     var totalResults = 0
     var category = "general"
     var isLoading = false
-    var lastVisibleItemPosition = 0
     var lastCategory: String? = null
     var linearProgressBar: ProgressBar? = null
+    var searchView: SearchView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val progressBar = findViewById<ProgressBar>(R.id.pgBarMain)
         linearProgressBar = findViewById(R.id.linearProgressBar)
+
+        adapter = NewsAdapter(this@MainActivity, articles)
+        val newsList = findViewById<RecyclerView>(R.id.newsList)
+
+        val layoutManager = LinearLayoutManager(this@MainActivity)
+        newsList.layoutManager = layoutManager
+        newsList.adapter = adapter
+
+
+        //Logic of Search view
+        searchView = findViewById(R.id.searchView)
+
+        // Use SearchView.OnQueryTextListener directly
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (!query.isNullOrBlank()) {
+                    performSearch(query)
+                    return true
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // Handle text change if needed
+                return false
+            }
+        })
 
         val buttonIds = arrayOf(
             R.id.button1,
@@ -48,79 +75,55 @@ class MainActivity : AppCompatActivity() {
             R.id.button7
         )
 
-
-
         val commonClickListener = View.OnClickListener { view ->
             when (view.id) {
                 R.id.button1 -> {
                     category = "business"
-                    getNews("in", category)
+                    getHeadlines(category)
                 }
+
                 R.id.button2 -> {
                     category = "entertainment"
-                    getNews("in", category)
+                    getHeadlines(category)
                 }
+
                 R.id.button3 -> {
                     category = "general"
-                    getNews("in", category)
+                    getHeadlines(category)
                 }
+
                 R.id.button4 -> {
                     category = "health"
-                    getNews("in", category)
+                    getHeadlines(category)
                 }
+
                 R.id.button5 -> {
                     category = "science"
-                    getNews("in", category)
+                    getHeadlines(category)
                 }
+
                 R.id.button6 -> {
                     category = "sports"
-                    getNews("in", category)
+                    getHeadlines(category)
                 }
+
                 R.id.button7 -> {
                     category = "technology"
-                    getNews("in", category)
+                    getHeadlines(category)
                 }
             }
         }
-
 
         buttonIds.forEach { buttonId ->
             val button = findViewById<Button>(buttonId)
             button.setOnClickListener(commonClickListener)
         }
 
-        adapter = NewsAdapter(this@MainActivity, articles)
-        val newsList = findViewById<RecyclerView>(R.id.newsList)
-
-        val layoutManager = LinearLayoutManager(this@MainActivity)
-        newsList.layoutManager = layoutManager
-        newsList.adapter = adapter
-
-        newsList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-
-                val visibleItemCount = layoutManager.childCount
-                val totalItemCount = layoutManager.itemCount
-                lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
-
-                // Logic of Scrolling down, load more news only if there are more items to load and not currently loading
-                if (dy > 0 && !isLoading && lastVisibleItemPosition >= totalItemCount - 1) {
-                    Log.d("last-item", "$visibleItemCount")
-
-                    progressBar.visibility = View.VISIBLE
-                    // Increment pageNum for pagination
-                    pageNum++
-                    getNews("in", category)
-                }
-            }
-        })
-
         // Initial API call
-        getNews("in", category)
+        getHeadlines(category)
     }
 
-    private fun getNews(country: String, newCategory: String) {
+    private fun getHeadlines(newCategory: String) {
         isLoading = true
 
         linearProgressBar?.visibility = View.VISIBLE
@@ -131,12 +134,13 @@ class MainActivity : AppCompatActivity() {
             lastCategory = newCategory
         }
 
-        val news = NewsService.newsInstance.getHeadlines(country, newCategory, pageNum)
+        val news = NewsService.newsInstance.getHeadlines(country="in", newCategory, pageNum)
 
         news.enqueue(object : Callback<News> {
             override fun onFailure(call: Call<News>, t: Throwable) {
                 Log.d("Suraj", "Error in fetching news: ${t.message}")
-                Toast.makeText(this@MainActivity, "Error in fetching news", Toast.LENGTH_LONG).show()
+                Toast.makeText(this@MainActivity, "Error in fetching news", Toast.LENGTH_LONG)
+                    .show()
                 isLoading = false
                 linearProgressBar?.visibility = View.GONE
             }
@@ -151,11 +155,61 @@ class MainActivity : AppCompatActivity() {
 
                     articles.addAll(news.articles)
                     adapter.notifyDataSetChanged()
-                    Log.d("Suraj", "Total items: ${adapter.itemCount}, Total results: $totalResults")
+                    Log.d(
+                        "Suraj",
+                        "Total items: ${adapter.itemCount}, Total results: $totalResults"
+                    )
                 }
                 isLoading = false
                 linearProgressBar?.visibility = View.GONE
             }
         })
+    }
+
+    private fun getNews(query: String) {
+        isLoading = true
+
+        linearProgressBar?.visibility = View.VISIBLE
+
+        // Clear existing articles
+        articles.clear()
+        lastCategory = null
+
+        val news = NewsService.newsInstance.searchNews(query)
+
+        news.enqueue(object : Callback<News> {
+            override fun onResponse(call: Call<News>, response: Response<News>) {
+                val newsResponse = response.body()
+
+                if (newsResponse != null) {
+                    totalResults = newsResponse.totalResults
+                    val progressBar = findViewById<ProgressBar>(R.id.pgBarMain)
+                    progressBar.visibility = View.INVISIBLE
+
+                    articles.addAll(newsResponse.articles)
+                    adapter.notifyDataSetChanged()
+                    Log.d(
+                        "Suraj",
+                        "Total items: ${adapter.itemCount}, Total results: $totalResults"
+                    )
+                } else {
+                    Log.d("Suraj", "Empty response body")
+                }
+
+                isLoading = false
+                linearProgressBar?.visibility = View.GONE
+            }
+
+            override fun onFailure(call: Call<News>, t: Throwable) {
+                Log.d("Suraj", "Error in fetching news: ${t.message}")
+                Toast.makeText(this@MainActivity, "Error in fetching news", Toast.LENGTH_LONG).show()
+
+                isLoading = false
+                linearProgressBar?.visibility = View.GONE
+            }
+        })
+    }
+    private fun performSearch(query: String) {
+            getNews(query)
     }
 }
